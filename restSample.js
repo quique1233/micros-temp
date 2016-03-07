9e2a29566c04d2aceda38a3c103160c0775e60ce
Enrique Antezana <enrique.antezana@fundacion-jala.org>
2016-03-06 21:47:35 -0400

    Moment js package added

--- restSample.js
+++ restSample.js
@@ -2,6 +2,9 @@ if (Meteor.isClient) {
   Template.hello.helpers({
     temperatures: function () {
       return Temperatures.find({});
+    },
+    date: function() {
+      return moment(this.date).format('D/M/YYYY HH:mm');
     }
   });
 }
