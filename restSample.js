15ec08c997d2de9a86645be252b1b10f9e31b37d
Enrique Antezana <enrique.antezana@fundacion-jala.org>
2016-03-06 22:38:15 -0400

    Charts library added

--- restSample.js
+++ restSample.js
@@ -1,4 +1,52 @@
 if (Meteor.isClient) {
+  Template.hello.onRendered(function () {
+    this.autorun(function () {
+      let temperatures = Temperatures.find({}).fetch();
+      let values = [];
+      _.each(temperatures, function(temp) {
+        let arr = [];
+        arr.push(temp.date);
+        arr.push(temp.temperature);
+        values.push(arr);
+      });
+      let data = [
+        {
+          "key": "Series 1",
+          "values": values
+      }];
+
+      nv.addGraph(function() {
+        var chart = nv.models.cumulativeLineChart()
+          .x(function(d) { return d[0] })
+          .y(function(d) { return d[1] })
+          .color(d3.scale.category10().range())
+          .useInteractiveGuideline(true)
+          .forceY([0])
+          .margin({right: 30})
+          ;
+
+        chart.xAxis
+          .tickFormat(function(d) {
+            return d3.time.format('%d/%m/%Y')(new Date(d));
+          });
+
+        chart.yAxis
+        .axisLabel('ºC')
+        .tickFormat(d3.format('d'));
+
+        d3.select('#chart svg')
+          .datum(data)
+          .transition().duration(500)
+          .call(chart)
+          ;
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+      });
+    });
+  });
+
   Template.hello.helpers({
     temperatures: function () {
       return Temperatures.find({});
