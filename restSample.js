2c419c9db1e5490e4662c272e5fcf82aa7f228a7
Enrique Antezana <Enrique.Antezana@fundacion-jala.org>
2016-03-12 22:33:39 -0400

    Charts for other data added

--- restSample.js
+++ restSample.js
@@ -1,45 +1,72 @@
 if (Meteor.isClient) {
   Template.hello.onCreated(function() {
     this.checkchart = new ReactiveVar(true);
+    this.chartFor = new ReactiveVar('temperature');
   });
   Template.hello.onRendered(function () {
     this.autorun(function () {
       Template.instance().checkchart.get();
-      let temperatures = Temperatures.find({}, {sort: {date: -1}, limit: 100 }).fetch();
-      temperatures = _.uniq(temperatures, function(temp) {
-        return Math.round(temp.temperature);
-      });
+      let chartFor = Template.instance().chartFor.get();
+      let temperatures = Temperatures.find({}, {sort: {date: -1}, limit: 10 }).fetch();
+      let configuration = {};
+      switch (chartFor) {
+        case 'temperature':
+          configuration.color = "#FE2E2E";
+          configuration.ranges = [-10,-5, 0, 5,10,15,20,25,30,35,40];
+          configuration.attr = chartFor;
+          configuration.ylabel = "ºC";
+          configuration.label = "temperatura";
+          break;
+        case 'humidity':
+          configuration.color = "#31B404";
+          configuration.ranges = [20,30, 40, 50, 60, 70,80,90,100];
+          configuration.attr = chartFor;
+          configuration.ylabel = "%";
+          configuration.label = "% humedad";
+          break;
+        case 'photoresistor':
+          configuration.color = "#FFFF00";
+          configuration.ranges = [];
+          configuration.attr = chartFor;
+          configuration.ylabel = "fotoresistor";
+          configuration.label = "fotoresistor";
+          break;
+      }
       let values = [];
       _.each(temperatures, function(temp) {
         let arr = [];
         arr.push(temp.date);
-        arr.push(temp.temperature);
+        arr.push(temp[configuration.attr]);
         values.push(arr);
       });
       let data = [
         {
-          "key": "Series 1",
-          "values": values
+          "key": configuration.label,
+          "values": values,
+          "color": configuration.color
       }];
 
+      console.log(data);
+
       nv.addGraph(function() {
-        var lineChart = nv.models.cumulativeLineChart()
+        var lineChart = nv.models.lineChart()
           .x(function(d) { return d[0] })
           .y(function(d) { return d[1] })
           .color(d3.scale.category10().range())
-          .useInteractiveGuideline(true)
-          .forceY([0])
+          .useInteractiveGuideline(false)
+          .forceY(configuration.ranges)
           .margin({right: 30})
           ;
 
         lineChart.xAxis
+          .showMaxMin(false)
           .tickFormat(function(d) {
             return d3.time.format('%d/%m/%Y')(new Date(d));
           });
 
         lineChart.yAxis
-        .axisLabel('ºC')
-        .tickFormat(d3.format('d'));
+        .axisLabel(configuration.label)
+        .tickFormat(d3.format(',.2f'));
 
         d3.select('#lineChart svg')
           .datum(data)
@@ -87,20 +114,29 @@ if (Meteor.isClient) {
       return Template.instance().checkchart.get();
     },
     allRecords: function() {
-      return Temperatures.find({});
+      return Temperatures.find({}, {sort: {date: -1}});
     }
   });
 
   Template.hello.events({
-    'click .ui.button.records': function() {
-      Template.instance().checkchart.set(false);
-    },
     'click .ui.button.graph': function() {
       Template.instance().checkchart.set(true);
     },
     'click #export': function() {
       // console.log('click');
       $("#table").tableToCSV();
+    },
+    'click .humidity': function() {
+      Template.instance().chartFor.set('humidity');
+    },
+    'click .temperature': function() {
+      Template.instance().chartFor.set('temperature');
+    },
+    'click .photoresistor': function() {
+      Template.instance().chartFor.set('photoresistor');      
+    },
+    'click .allrecords': function() {
+      Template.instance().checkchart.set(false);
     }
   })
 }
